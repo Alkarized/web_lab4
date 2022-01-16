ab1c59b685689f4699c39387bc506ca2b14b2a60
Alkarized <oskilko02@gmail.com>
2022-01-16 14:04:46 +0300

    add some code

--- src/main/java/ru/itmo/alkarized/lab4/Lab4Application.java
+++ src/main/java/ru/itmo/alkarized/lab4/Lab4Application.java
@@ -3,9 +3,10 @@ package ru.itmo.alkarized.lab4;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"ru.itmo.alkarized.lab4.configurations", "ru.itmo.alkarized.lab4.controllers",
+        "ru.itmo.alkarized.lab4.entities", "ru.itmo.alkarized.lab4.services"})
 public class Lab4Application {
-
+    
     public static void main(String[] args) {
         SpringApplication.run(Lab4Application.class, args);
     }
---
+++ src/main/java/ru/itmo/alkarized/lab4/entities/User.java
@@ -0,0 +1,8 @@
+package ru.itmo.alkarized.lab4.entities;
+
+import javax.persistence.Entity;
+
+@Entity
+public class User {
+    
+}
